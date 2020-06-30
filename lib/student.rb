87be1720b82acc8c45c53121e6bc0a08d0be704c
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-30 21:50:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -99,7 +99,7 @@ def self.first_student_in_grade_10
     SELECT *
     FROM students
     WHERE grade = 10
-    LIMIT 1
+    ORDER BY students.id LIMIT 1
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
