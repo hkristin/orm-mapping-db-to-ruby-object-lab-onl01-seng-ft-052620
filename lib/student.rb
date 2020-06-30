105bbf155cac350414cef47fe359e86948d529f2
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-30 21:34:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,7 @@ end
     WHERE grade = 9
     LIMIT 1
     SQL
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
   end
 end
